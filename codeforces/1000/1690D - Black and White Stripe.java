dea9910f3c9ecac229bb80c4979b6b4007706962
Chandan <chandu.h2024@gmail.com>
2026-08-21 20:48:37 +0530

    Add Black and White Stripe (1000)

--- codeforces/1000/1690D - Black and White Stripe.java
+++ codeforces/1000/1690D - Black and White Stripe.java
@@ -1,4 +1,66 @@
+// sliding window approach
+
 import java.io.*;
+import java.util.*;
+
+public class DBlackAndWhiteStripe {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            String s = br.readLine();
+
+            int left = 0;
+            int whites = 0;
+            int min = 0;
+
+            for (int i = 0; i < k; i++) {
+                if (s.charAt(i) == 'W') {
+                    whites++;
+                }
+            }
+            min = whites;
+            for (int i = k; i < s.length(); i++) {
+                if (s.charAt(i) == 'B') {
+                    if (s.charAt(left) == 'B') {
+                        left++;
+                    }
+                    else{
+                        whites--;
+                        min = Math.min(min, whites);
+                        left++;
+                    }
+                }
+                else{
+                    if (s.charAt(left) == 'W') {
+                        left++;
+                    }
+                    else{
+                        whites++;
+                        left++;
+                    }
+                }
+            }
+            System.out.println(min);
+        }
+    }
+}
+
+
+
+
+// TLE
+
+/* import java.io.*;
 import java.util.*;
 
 public class DBlackAndWhiteStripe {
@@ -10,37 +72,31 @@ public class DBlackAndWhiteStripe {
         int t = Integer.parseInt(br.readLine());
 
         while (t-- > 0) {
-
             StringTokenizer st = new StringTokenizer(br.readLine());
-
             int n = Integer.parseInt(st.nextToken());
             int k = Integer.parseInt(st.nextToken());
 
             String s = br.readLine();
-
-            int white = 0;
-
-            // First window
-            for (int i = 0; i < k; i++) {
-                if (s.charAt(i) == 'W')
-                    white++;
+            int res = Integer.MAX_VALUE;
+            for (int i = 0; i < n-k+1; i++) {
+                int count = 0;
+                int temp = 0;
+                for (int j = i; j < s.length(); j++) {
+                    if (count == k) {
+                        break;
+                    }
+                    if (s.charAt(j) == 'W') {
+                        temp++;
+                        count++;
+                    }
+                    else{
+                        count++;
+                    }
+                }
+                res = Math.min(res, temp);
             }
-
-            int ans = white;
-
-            // Slide the window
-            for (int i = k; i < n; i++) {
-
-                if (s.charAt(i - k) == 'W')
-                    white--;
-
-                if (s.charAt(i) == 'W')
-                    white++;
-
-                ans = Math.min(ans, white);
-            }
-
-            System.out.println(ans);
+            System.out.println(res);
         }
+
     }
-}
+} */
