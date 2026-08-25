9b95d8676fbf220e487fa78ee3200af90c82780d
Chandan <chandu.h2024@gmail.com>
2026-08-25 21:07:56 +0530

    Add Binary-Tree-Preorder-Traversal (tree)

---
+++ leetcode/tree/144-Binary-Tree-Preorder-Traversal.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        recursion(root, list);
+        return list;
+    }
+
+    public void recursion(TreeNode root, List<Integer> list){
+        if(root == null) return;
+
+        list.add(root.val);
+        recursion(root.left, list);
+        recursion(root.right, list);
+    }
+}
