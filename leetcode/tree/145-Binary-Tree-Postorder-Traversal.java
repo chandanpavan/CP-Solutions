91e610550588e204633fd0af1daa79d90feb0413
Chandan <chandu.h2024@gmail.com>
2026-08-28 17:35:32 +0530

    Add Binary-Tree-Postorder-Traversal (tree)

--- leetcode/tree/145-Binary-Tree-Postorder-Traversal.java
+++ leetcode/tree/145-Binary-Tree-Postorder-Traversal.java
@@ -13,31 +13,63 @@
  *     }
  * }
  */
-// Using one stack
 
+// Using one stack
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        Stack<TreeNode> stack1 = new Stack<>();
-        Stack<TreeNode> stack2 = new Stack<>();
+        Stack<TreeNode> st = new Stack<>();
         List<Integer> list = new ArrayList<>();
-        if(root ==  null) return list;
-
-        stack1.push(root);
-        while(!stack1.isEmpty()){
-            root = stack1.pop();
-            if(root.left != null) stack1.push(root.left);
-            if(root.right != null) stack1.push(root.right);
-
-            stack2.push(root);
-        }
-        
-        while(!stack2.isEmpty()){
-            list.add(stack2.peek().val);
-            stack2.pop();
+        if(root == null) return list;
+        TreeNode node = root;
+        while(node != null || !st.isEmpty()){
+            if(node != null){
+                st.push(node);
+                node = node.left;
+            }
+            else{
+                TreeNode temp = st.peek().right;
+                if(temp == null) {
+                    temp = st.pop();
+                    list.add(temp.val);
+                    while(!st.isEmpty() && st.peek().right == temp){
+                        temp = st.pop();
+                        list.add(temp.val);
+                    }
+                }
+                else{
+                    node = temp;
+                }
+            }
         }
         return list;
-    }   
+    }
 }
+
+// Using two stack
+
+// class Solution {
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         Stack<TreeNode> stack1 = new Stack<>();
+//         Stack<TreeNode> stack2 = new Stack<>();
+//         List<Integer> list = new ArrayList<>();
+//         if(root ==  null) return list;
+
+//         stack1.push(root);
+//         while(!stack1.isEmpty()){
+//             root = stack1.pop();
+//             if(root.left != null) stack1.push(root.left);
+//             if(root.right != null) stack1.push(root.right);
+
+//             stack2.push(root);
+//         }
+        
+//         while(!stack2.isEmpty()){
+//             list.add(stack2.peek().val);
+//             stack2.pop();
+//         }
+//         return list;
+//     }   
+// }
 
 // recursive solution
 
