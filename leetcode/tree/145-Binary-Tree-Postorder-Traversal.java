ad78a7e7087836e6b39b2ce44b5557f3d0e0df37
Chandan <chandu.h2024@gmail.com>
2026-08-28 13:38:22 +0530

    Add Binary-Tree-Postorder-Traversal (tree)

--- leetcode/tree/145-Binary-Tree-Postorder-Traversal.java
+++ leetcode/tree/145-Binary-Tree-Postorder-Traversal.java
@@ -13,17 +13,45 @@
  *     }
  * }
  */
+// Using one stack
+
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
         List<Integer> list = new ArrayList<>();
-        recursion(list, root);
-        return list;
-    }
-    public static void recursion(List<Integer> list, TreeNode root){
-        if(root == null) return;
+        if(root ==  null) return list;
 
-        recursion(list,root.left);
-        recursion(list,root.right);
-        list.add(root.val);
-    }
+        stack1.push(root);
+        while(!stack1.isEmpty()){
+            root = stack1.pop();
+            if(root.left != null) stack1.push(root.left);
+            if(root.right != null) stack1.push(root.right);
+
+            stack2.push(root);
+        }
+        
+        while(!stack2.isEmpty()){
+            list.add(stack2.peek().val);
+            stack2.pop();
+        }
+        return list;
+    }   
 }
+
+// recursive solution
+
+// class Solution {
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         List<Integer> list = new ArrayList<>();
+//         recursion(list, root);
+//         return list;
+//     }
+//     public static void recursion(List<Integer> list, TreeNode root){
+//         if(root == null) return;
+
+//         recursion(list,root.left);
+//         recursion(list,root.right);
+//         list.add(root.val);
+//     }
+// }
