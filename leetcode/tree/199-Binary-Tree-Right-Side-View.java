7c90e8d617001c774e5a6ab03edf6411b492ce72
Chandan <chandu.h2024@gmail.com>
2026-09-02 19:40:22 +0530

    Add Binary-Tree-Right-Side-View (tree)

---
+++ leetcode/tree/199-Binary-Tree-Right-Side-View.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        recursion(root, 0, list);
+        return list;
+    }
+
+    public static void recursion(TreeNode root, int level , List<Integer> list){
+        if(root == null) return;
+        if(level == list.size()) list.add(root.val);
+
+        recursion(root.right,level+1,list);
+        recursion(root.left,level+1,list);
+    }
+}
