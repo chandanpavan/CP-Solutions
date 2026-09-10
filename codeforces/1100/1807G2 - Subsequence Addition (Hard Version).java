d20fff252642892db9289c63ca7506fb74dd0c3e
Chandan <chandu.h2024@gmail.com>
2026-09-10 18:33:03 +0530

    Add Subsequence Addition (Hard Version) (1100)

---
+++ codeforces/1100/1807G2 - Subsequence Addition (Hard Version).java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+
+public class G2SubsequenceAdditionHardVersion {
+
+    static final class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokens = new StringTokenizer("");
+
+        FastScanner(InputStream input) {
+            reader = new BufferedReader(new InputStreamReader(input));
+        }
+
+        String next() throws IOException {
+            while (!tokens.hasMoreTokens())
+                tokens = new StringTokenizer(reader.readLine());
+            return tokens.nextToken();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        FastScanner fs = new FastScanner(System.in);
+        int testCases = fs.nextInt();
+
+        while (testCases-- > 0) {
+            int n = fs.nextInt();
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                arr[i] = fs.nextInt();
+            }
+
+            Arrays.sort(arr);
+
+            long[] prefixSum = new long[n];
+            boolean flag = false;
+            // prefixSum[0] = 1;
+            if (n == 1) {
+                if (arr[0] != 1) {
+                    flag = true;
+                }
+            }
+            if (arr[0] != 1 || (n > 1 && arr[1] != 1)) {
+                flag = true;
+            }
+            for (int i = 1; i < arr.length; i++) {
+                prefixSum[i] = prefixSum[i - 1] + arr[i - 1];
+                if (prefixSum[i] < arr[i])
+                    flag = true;
+            }
+
+            if (flag) {
+                System.out.println("NO");
+            } else {
+                System.out.println("YES");
+            }
+
+        }
+
+    }
+}
